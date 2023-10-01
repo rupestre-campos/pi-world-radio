dd408a5c1204b4d4cc9b4235822fa8e0830f92d4
Caina K. Campos <rupestre.campos@gmail.com>
2023-10-01 08:02:27 -0400

    reuse locations data

--- radio_cli.py
+++ radio_cli.py
@@ -6,11 +6,17 @@ import subprocess
 import time
 import json
 import unicodedata
+import os
 
-
-default_file_path = "./app/data/geo_json.min.json"
+default_file_path = "/tmp/geo_json.min.json"
 geojson_url = f"https://radio.garden/api/ara/content/places"
 
+def clear():
+    try:
+        subprocess.run(["clear"])
+        subprocess.run(["cls" ])
+    except:
+        pass
 
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
@@ -22,8 +28,11 @@ def get_feature(d):
     feature['properties'] = {'title': remove_accents(d['title']), 'country': remove_accents(d['country']), 'location_id': d['id'], 'lng': d['geo'][0], 'lat': d['geo'][1]}
     return feature
 
-def fetch_geojson_data(url, default_file_path):
-    # todo add cache layer
+def fetch_geojson_data(url, default_file_path, update=True):
+    if os.path.exists(default_file_path) and update==False:
+        with open(default_file_path) as file_open:
+            geo_json = json.load(file_open)
+            return geo_json
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise exception for bad requests
@@ -69,15 +78,13 @@ def list_stations(geojson_data, selected_country, selected_city):
             return data["data"]["content"][0]["items"]
 
 def play_stream(stream_url):
-    print(" use  / * to decrease increase volume" )
-    print(" use m to mute and q to quit" )
+    print("Hit  / to decrease and * to increase volume" )
+    print("Hit space to pause, m to mute and q to quit" )
     try:
         # Use subprocess to execute the MPlayer command with the stream URL
         subprocess.run(['mpv',
                         '--cache-pause-initial=yes',
                         '--cache-pause=no',
-                        #'--cache-default=15360',
-                        #'--cache-initial=256',
                         '--demuxer-thread=yes',
                         '--demuxer-readahead-secs=30',
                         '--framedrop=vo',
@@ -88,32 +95,40 @@ def play_stream(stream_url):
         print(f"Error: {e}")
 
 def main():
-
-    geojson_data = fetch_geojson_data(geojson_url, default_file_path)
+    clear()
+    print("Welcome to radio.garden in a cli")
+    update_data = False
+    update_locations = input(" wanna update locations? [Y/n]" )
+    if update_locations == "Y":
+        update_data = True
+    geojson_data = fetch_geojson_data(geojson_url, default_file_path, update_data)
 
     if geojson_data:
         countries = list_countries(geojson_data)
         country_completer = WordCompleter(countries, ignore_case=True)
+        print(" Hit Tab for a complete list or just start typing the names" )
         selected_country = prompt('Select a country: ', completer=country_completer)
-
+        if selected_country not in countries:
+            print(" Country not in list, exiting..." )
+            return None
         cities = list_cities(geojson_data, selected_country)
         city_completer = WordCompleter(cities, ignore_case=True)
         selected_city = prompt('Select a city: ', completer=city_completer)
-
+        if selected_city not in cities: 
+            print("City not in list, exiting..." )
+            return None
         stations = list_stations(geojson_data, selected_country, selected_city)
         station_completer = WordCompleter([station['title'] for station in stations], ignore_case=True)
         selected_station = prompt('Select a station: ', completer=station_completer)
 
         station_data = next((station for station in stations if station['title'] == selected_station), None)
-        if station_data:
-            channel_id = station_data["href"].split("/")[-1]
-            val = int(time.time() * 1000)
-            stream_url = f"https://radio.garden/api/ara/content/listen/{channel_id}/channel.mp3?{val}"
-            print(f"Streaming URL: {stream_url}")
-            play_stream(stream_url)
-        else:
-            print("Invalid station selection.")
-
+        if not station_data: 
+            print(" Station not in list, exiting..." )
+        channel_id = station_data["href"].split("/")[-1]
+        val = int(time.time() * 1000)
+        stream_url = f"https://radio.garden/api/ara/content/listen/{channel_id}/channel.mp3?{val}"
+        #print(f"Streaming URL: {stream_url}")
+        play_stream(stream_url)
 
 if __name__ == "__main__":
     main()
